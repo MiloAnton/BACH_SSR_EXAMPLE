d762329f0bf1d6b76d087aa074e8afc038292740
Milo Roche-Vandenbroucque <milo.roche@outlook.com>
2023-11-14 14:31:04 +0100

    [frontend] Added Request to get all livres

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
-import './App.css';
+import "./App.css";
 import Topbar from "./components/topbar";
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
+  // Stock de livres en local
+  const [books, setBooks] = useState([]);
+
   // Implémentation de la logique du bouton cliquez-moi
   const [compteur, setCompteur] = useState(0);
   const handleClick = () => {
     setCompteur(compteur + 1);
-  }
+  };
   // Implémentation de la logique de reset du compteur
   const Reset = () => {
-    setCompteur(0)
-  }
+    setCompteur(0);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:4000/books") // Remplacez par l'URL de votre API
+      .then((response) => response.json())
+      .then((data) => setBooks(data))
+      .catch((error) =>
+        console.error("Erreur lors du chargement des livres:", error)
+      );
+  }, []);
 
   return (
     <>
@@ -24,6 +36,16 @@ function App() {
         <button className="bouton" onClick={Reset}>
           Reset
         </button>
+        <div>
+          <h2>Liste des livres</h2>
+          <ul>
+            {books.map((book) => (
+              <li key={book.id}>
+                {book.title} - {book.author}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
