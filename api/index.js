cdc4dbc2745912a6a05a0df4c013c3790a248f09
Milo Roche-Vandenbroucque <milo.roche@outlook.com>
2023-11-15 09:08:31 +0100

    [frontend] Added quick add (wip)

--- api/index.js
+++ api/index.js
@@ -1,29 +1,48 @@
-const express = require('express');
-const { Pool } = require('pg');
-const cors = require('cors');
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
 
 const app = express();
+app.use(express.json())
 const port = 4000;
 
 app.use(cors());
 
 // Configuration de la connexion à la base de données
 const pool = new Pool({
-  user: 'user', // Votre nom d'utilisateur pour PostgreSQL
-  host: 'localhost', // L'adresse de l'hôte de la base de données
-  database: 'librarydb', // Le nom de votre base de données
-  password: 'password', // Votre mot de passe pour PostgreSQL
+  user: "user", // Votre nom d'utilisateur pour PostgreSQL
+  host: "localhost", // L'adresse de l'hôte de la base de données
+  database: "librarydb", // Le nom de votre base de données
+  password: "password", // Votre mot de passe pour PostgreSQL
   port: 5432, // Le port sur lequel PostgreSQL écoute
 });
 
-// Un exemple de route qui interroge la base de données et récupère tout les livres
-app.get('/books', async (req, res) => {
+// Création d'une route pour l'ajout d'un livre dans la BDD
+app.post("/books", async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM books');
+    const title = req.body.title;
+    const author = req.body.author;
+    const published_date = req.body.published_date;
+    const genre = req.body.genre;
+    const query =
+      "INSERT INTO books (title, author, published_date, genre) VALUES ($1, $2, $3, $4)"; //Je les injecte dans une requête SQL pour qu'elles rentrent en BDD
+    const values = [title, author, published_date, genre];
+    await pool.query(query, values);
+    res.status(201).send("Livre ajouté avec succès !"); //Message de confirmation "Félicitations, ça a marché."
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erreur du serveur");
+  }
+});
+
+// Un exemple de route qui interroge la base de données et récupère tout les livres
+app.get("/books", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM books");
     res.json(rows);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Erreur du serveur');
+    res.status(500).send("Erreur du serveur");
   }
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,12 @@ function App() {
   // Stock de livres en local
   const [books, setBooks] = useState([]);
 
+  // Stock du livre à créer
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [published_date, setPublished] = useState('');
+  const [genre, setGenre] = useState('');
+
   // Implémentation de la logique du bouton cliquez-moi
   const [compteur, setCompteur] = useState(0);
   const handleClick = () => {
@@ -30,6 +36,31 @@ function App() {
       );
   }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const bookData = {
+      title,
+      author,
+      published_date,
+      genre,
+    };
+
+    fetch('http://localhost:4000/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bookData),
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Not ok');
+    })
+  }
+
   return (
     <>
       <Topbar />
@@ -40,6 +71,36 @@ function App() {
         </button>
         <button className="bouton" onClick={Reset}>
           Reset
+        </button>
+        <div>
+          <h2>Ajouter un nouveau livre</h2>
+          <input
+            type="text"
+            placeholder="Titre"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Auteur"
+            value={author}
+            onChange={e => setAuthor(e.target.value)}
+          />
+          <input
+            type="date"
+            placeholder="Date de publication"
+            value={published_date}
+            onChange={e => setPublished(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Genre"
+            value={genre}
+            onChange={e => setGenre(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSubmit}>
+          Ajouter le livre
         </button>
         <div>
           <h2>Liste des livres</h2>
